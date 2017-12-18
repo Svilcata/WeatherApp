7e81e5671439c4d2bd18f3dc3930fb15caef18ac
Svilen Strahilov <Svilen.Strahilov@adastragrp.com>
2017-12-18 17:27:38 +0200

    updated api interface, created layout for items and updated activity_main, added api key to strings

--- app/src/main/java/com/example/svilenstrahilov/weatherapp/retrofit/ApiInterface.java
+++ app/src/main/java/com/example/svilenstrahilov/weatherapp/retrofit/ApiInterface.java
@@ -1,5 +1,12 @@
 package com.example.svilenstrahilov.weatherapp.retrofit;
 
+import com.google.gson.JsonObject;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface ApiInterface {
-    
+    @GET("premium/v1/weather.ashx?")
+    Call<JsonObject> response(@Query("key") String api_key, @Query("q") String city, @Query("format") String format);
 }
