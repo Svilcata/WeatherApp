0aa71f6b8d4cd6ddd131cb70dec9a82c33d51753
Svilen Strahilov <Svilen.Strahilov@adastragrp.com>
2017-12-21 15:07:28 +0200

    fixed weathericonurlconverter.class

--- app/src/main/java/com/example/svilenstrahilov/weatherapp/data/models/converters/WeatherIconUrlConverter.java
+++ app/src/main/java/com/example/svilenstrahilov/weatherapp/data/models/converters/WeatherIconUrlConverter.java
@@ -1,8 +1,27 @@
 package com.example.svilenstrahilov.weatherapp.data.models.converters;
 
+import android.arch.persistence.room.TypeConverter;
+
+import com.example.svilenstrahilov.weatherapp.retrofit.data.WeatherIconUrl;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WeatherIconUrlConverter {
-//    @TypeConverter
-//    public static List<WeatherIconUrl> toList(String value) {
-//
-//    }
+    @TypeConverter
+    public static List<WeatherIconUrl> toList(String value) {
+        List<WeatherIconUrl> weatherIconUrls = new ArrayList<>();
+        weatherIconUrls.add(new WeatherIconUrl(value));
+        return weatherIconUrls;
+    }
+
+    @TypeConverter
+    public static String toString(List<WeatherIconUrl> weatherIconUrls) {
+        StringBuilder value = new StringBuilder();
+        for (WeatherIconUrl weatherIconUrl :
+                weatherIconUrls) {
+            value.append(weatherIconUrl.getWeatherIconUrl()).append(",");
+        }
+        return value.toString();
+    }
 }
